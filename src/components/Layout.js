3e9699935a6929cd7a6022e0004b515f38e1be1f
lippe chounyou <lippe6969@gmail.com>
2024-12-22 21:49:16 +0700

    Edit Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 function Layout() {
   return (
     <div>
-      {/* Navbar */}
-      <nav>
-        {/* ... */}
-      </nav>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+              ร้านหนังสือออนไลน์
+            </Link>
+          </Typography>
+          <Button color="inherit" component={Link} to="/login">
+            เข้าสู่ระบบ
+          </Button>
+        </Toolbar>
+      </AppBar>
 
-      {/* Content */}
       <Outlet />
-
-      {/* Footer */}
-      <footer>
-        {/* ... */}
-      </footer>
     </div>
   );
 }
