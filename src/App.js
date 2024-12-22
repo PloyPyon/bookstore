e822756b9886dc14c3deec9c45afbabd83d7c256
lippe chounyou <lippe6969@gmail.com>
2024-12-22 21:41:14 +0700

    Edit2 App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}> 
           <Route path="login" element={<LoginPage />} />
           <Route index element={<BookListPage />} />
           <Route
