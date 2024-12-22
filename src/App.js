cacd2abd6c4f0d41dda542d11e608400148f4def
lippe chounyou <lippe6969@gmail.com>
2024-12-22 21:36:06 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -6,34 +6,33 @@ import BookListPage from './components/BookListPage';
 import AddBookPage from './components/AddBookPage';
 import EditBookPage from './components/EditBookPage';
 
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/login" element={<LoginPage />} />
-          <Route index element={<BookListPage />} /> 
-          <Route
-            path="/add"
-            element={
-              <ProtectedRoute>
-                <AddBookPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit/:id"
-            element={
-              <ProtectedRoute>
-                <EditBookPage />
-              </ProtectedRoute>
-            }
-          />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route index element={<BookListPage />} />
+          <Route
+            path="add"
+            element={
+              <ProtectedRoute>
+                <AddBookPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="edit/:id"
+            element={
+              <ProtectedRoute>
+                <EditBookPage />
+              </ProtectedRoute>
+            }
+          />
         </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
